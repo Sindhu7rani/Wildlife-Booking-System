4544ebe9ba5cef44c8ce1a06eb0d4da580796fc3
Sindhu7rani <sindhu7rani@gmail.com>
2025-09-10 22:16:48 +0530

    First commit - Wildlife Booking System project

---
+++ src/main/java/com/wild_tour/dao/BookingDAO.java
@@ -0,0 +1,16 @@
+package com.wild_tour.dao;
+
+import java.util.ArrayList;
+
+import com.wild_tour.dto.Booking;
+
+public interface BookingDAO {
+	
+	public boolean insertBooking(Booking b);
+    public boolean updateBooking(Booking b);
+    public boolean deleteBooking(int bookingId);
+    public Booking getBooking(int bookingId);
+    public ArrayList<Booking> getAllBookings();
+
+
+}
